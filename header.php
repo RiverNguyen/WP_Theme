6b9bf21ed8ba0d8676a7578911f1e437d57cb3ce
Giang Nguyen Dinh <ngiang1309@gmail.com>
2025-03-26 14:33:39 +0700

    Update image paths and refactor navigation in HTML files

--- header.php
+++ header.php
@@ -7,11 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">
-    <link rel="shortcut icon" href="images/logo.png">
+    <link rel="shortcut icon" href="/wp-content/themes/jang/assets/images/logo.png">
 
-    <?php
-    wp_head();
-    ?>
+    <?php wp_head(); ?>
 
 </head>
 
@@ -29,23 +27,16 @@
         <div id="navigation" class="collapse navbar-collapse flex-column" >
             <img class="mb-3 mx-auto logo" src="images/logo.png" alt="logo" >
 
-            <ul class="navbar-nav flex-column text-sm-center text-md-left">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.html"><i class="fas fa-home fa-fw mr-2"></i>Blog Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="post.html"><i class="fas fa-file-alt fa-fw mr-2"></i>Blog Post</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="page.html"><i class="fas fa-file-image fa-fw mr-2"></i>Blog Page</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="archive.html"><i class="fas fa-archive fa-fw mr-2"></i>Blog Archive</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link btn btn-primary" href="contact.html"><i class="fas fa-envelope fa-fw mr-2"></i>Contact Us</a>
-                </li>
-            </ul>
+            <?php
+                wp_nav_menu([
+                "menu" => "primary",
+                "container" => "",
+                "theme_location" => "primary",
+                "items_wrap" =>
+                    '<ul id="" class="navbar-nav flex-column text-sm-center text-md-left">%3$s</ul>',
+            ]);
+            ?>
+
             <hr>
             <ul class="social-list list-inline py-3 mx-auto">
                 <li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
@@ -61,5 +52,5 @@
 
 <div class="main-wrapper">
     <header class="page-title theme-bg-light text-center gradient py-5">
-        <h1 class="heading"><?php the_title() ?></h1>
+        <h1 class="heading"><?php the_title(); ?></h1>
     </header>
